dbb6b32b4b8bdd2a24d05763adac8cdb9f190113
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-04-05 17:36:06 -0300

    Add project files.

---
+++ tiposDeTriangulosPOO/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace tiposDeTriangulosPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do { 
+            Console.Clear();
+
+            Triangulo triangulo = new Triangulo();
+
+            Console.Write("Digite o primeiro lado do Triângulo: ");
+            triangulo.lado1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o segundo lado do Triângulo: ");
+            triangulo.lado2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o terceiro lado do Triângulo: ");
+            triangulo.lado3 = Convert.ToInt32(Console.ReadLine());
+
+            triangulo.tipo = triangulo.GerarTipo();
+            Console.WriteLine(triangulo.tipo);
+
+            Console.ReadLine();
+            } while (true);
+        }
+    }
+}
---
+++ tiposDeTriangulosPOO/Triangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tiposDeTriangulosPOO
+{
+    internal class Triangulo
+    {
+        internal int lado1, lado2, lado3;
+        internal string tipo;
+
+        public string GerarTipo()
+        {
+                if (lado1 + lado2 < lado3 || lado2 + lado3 < lado1 || lado1 + lado3 < lado2)
+                {
+                    return "\nNão é um triângulo";
+                }
+                else if (lado1 == lado2 && lado2 == lado3)
+                {
+                    return "\nTriângulo Equilátero.";
+                }
+                else if (lado1 == lado2 || lado2 == lado3 || lado3 == lado1)
+                {
+                    return "\nTriângulo Isóceles";
+                }
+                else
+                {
+                    return "\nTriângulo Escalêno";
+                }
+        }
+    }
+}
